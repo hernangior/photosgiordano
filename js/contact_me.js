e916221cab37b709c9f57b28bee63650cbd64fc1
Hernandez <hernangior@gmail.com>
2020-03-16 15:53:05 -0300

    teste
    teste

--- js/contact_me.js
+++ js/contact_me.js
@@ -39,14 +39,25 @@ $(function() {
                     $('#contactForm').trigger("reset");
                 },
                 error: function() {
-                    // Fail message
+/*                    // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Desculpe " + firstName + ", parece que meu servidor de email não está respondendo. Por favor tente novamente mais tarde!");
+                    $('#success > .alert-danger').append("<strong>Desculpe " + firstName + 
+                        ", parece que meu servidor de email não está respondendo. Por favor tente novamente mais tarde!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
-                    $('#contactForm').trigger("reset");
+                    $('#contactForm').trigger("reset");*/
+
+
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Sua mensagem foi enviada. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+                    
                 },
             })
         },
