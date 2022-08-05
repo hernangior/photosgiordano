5dfb98d4b24de7c44872682286334747fd629401
Hernandez Giordano <hernangior@gmail.com>
2022-08-05 10:23:11 -0300

    new
    new

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,5 +1,18 @@
 $(function() {
 
+    //jQuery.support.cors = true;
+
+    function showSuccessMsg(){
+        // Success message
+        $('#success').html("<div class='alert alert-success'>");
+        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+        $('#success > .alert-success')
+            .append("<strong>Sua mensagem foi enviada. </strong>");
+        $('#success > .alert-success')
+            .append('</div>');
+    }
+
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
@@ -19,14 +32,23 @@ $(function() {
             }
 
             $.ajax({
-                url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScSb0xiEF75ZZwf9WGCbX6OSu6uhuM6LqP-iYrmbMaHjate-Q/formResponse",
                 type: "POST",
+                //dataType: 'json',
+                //crossDomain: true,
+                //headers: { 'Access-Control-Allow-Origin': "*"},
                 data: {
                     "entry.52904792": name,
                     "entry.1665733173": email,
-                    "entry.1129926948": message
+                    "entry.1129926948": message,
+
+                    //dlut: "1659705172628",
+                    //fvv: 1,
+                    //partialResponse: '[null,null,"7385271245913055562"]',
+                    //pageHistory: 0,
+                    //fbzx: '7385271245913055562'
                 },
                 cache: false,
+                url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScSb0xiEF75ZZwf9WGCbX6OSu6uhuM6LqP-iYrmbMaHjate-Q/formResponse",
                 success: function() {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
@@ -36,19 +58,25 @@ $(function() {
                         .append("<strong>Sua mensagem foi enviada. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
-
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Desculpe " + firstName + ", parece que estou com problemas. Por favor tente novamente mais tarde!");
-                    $('#success > .alert-danger').append('</div>');
+                error: function(error) {
+
+                    if ((!error.responseText) && (!error.status)){
+                        showSuccessMsg()
+                    }else{
+                        // Fail message
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-danger').append("<strong>Desculpe " + firstName + ", parece que estou com problemas. Por favor tente novamente mais tarde!");
+                        $('#success > .alert-danger').append('</div>');
+                    }
+
                     //clear all fields
                     $('#contactForm').trigger("reset");
+
                 },
             })
 
